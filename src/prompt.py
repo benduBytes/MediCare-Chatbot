b2f4c4c6e6d0d777f362ab88275dd1a0a8df18e8
Benedict Paul <118711640+benduBytes@users.noreply.github.com>
2025-05-03 01:45:16 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -1,16 +1,18 @@
 
+
+
 system_prompt = (
     "You are an assistant for question-answering tasks. "
-    "Use the provided context to answer the user's question. "
-    "If you don't know the answer, say you don't know. "
-    "Keep your response concise, within three sentences."
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
 )
 # system_prompt = (
-# "You are an assistant for question-answering tasks."
-# "Use the following pieces of retrieved context to answer"
-# "the question. If you don't know the answer, say that you"
-# "don't know. Use three sentences maximum and keep the"
-# "answer concise."
-# "\n\n"
-# "{context}"
+#     "You are an assistant for question-answering tasks. "
+#     "Use the provided context to answer the user's question. "
+#     "If you don't know the answer, say you don't know. "
+#     "Keep your response concise, within three sentences."
 # )
