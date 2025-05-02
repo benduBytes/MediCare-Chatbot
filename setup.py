565e7e345ddbfe46468f908c4b6913e81708b24f
Benedict Paul <118711640+benduBytes@users.noreply.github.com>
2025-05-03 01:09:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Benedict Paul',
+    author_email= 'benedict2003paul@gmail.com',
     packages= find_packages(),
     install_requires = []
 
